d3cf785130fa801fccbf43a8d39d9c1842dabebb
Luu476 <villegasmarialuly@gmail.com>
2025-06-14 12:48:12 -0300

    ..
    ..

--- controllers/turnos.controller.js
+++ controllers/turnos.controller.js
@@ -3,12 +3,19 @@ import pool from '../config/db.js';
 export const mostrarFormularioTurno = async (req, res) => {
   try {
     const [medicos] = await pool.query('SELECT dni, nombre, especialidad FROM medicos');
-    res.render('turnos', { medicos });
+    const [turnos] = await pool.query(`
+      SELECT * FROM turnos 
+      WHERE CONCAT(fecha_turno, ' ', hora_turno) >= NOW()
+      ORDER BY fecha_turno, hora_turno
+    `);
+    const error = req.query.error || null;
+    res.render('turnos', { medicos, error, turnos }); 
   } catch (error) {
     console.error('Error al cargar médicos:', error);
     res.status(500).send('Error interno');
   }
 };
+
 
 export const registrarTurno = async (req, res) => {
   try {
@@ -25,7 +32,22 @@ export const registrarTurno = async (req, res) => {
       dni_medico
     } = req.body;
 
+    const fechaHoraTurno = new Date(`${fecha_turno}T${hora_turno}`);
+    const ahora = new Date();
 
+    if (fechaHoraTurno < ahora) {
+      return res.redirect('/turnos?error=No se puede agendar un turno en el pasado');
+    }
+
+    // Mover esta línea ANTES de usarla
+    const [existente] = await pool.query(
+      `SELECT * FROM turnos WHERE dni_medico = ? AND fecha_turno = ? AND hora_turno = ?`,
+      [dni_medico, fecha_turno, hora_turno]
+    );
+
+    if (existente.length > 0) {
+      return res.redirect('/turnos?error=Ya hay un turno asignado para ese médico en esa fecha y hora');
+    }
 
     await pool.query(`
       INSERT INTO turnos (
@@ -55,7 +77,12 @@ export const registrarTurno = async (req, res) => {
 
 export const listarTurnos = async (req, res) => {
   try {
-    const [turnos] = await pool.query('SELECT * FROM turnos ORDER BY fecha_turno, hora_turno');
+    const [turnos] = await pool.query(`
+      SELECT * FROM turnos 
+      WHERE CONCAT(fecha_turno, ' ', hora_turno) >= NOW()
+      ORDER BY fecha_turno, hora_turno
+    `);
+
     res.render('lista_turnos', { turnos });
   } catch (error) {
     console.error('Error al listar los turnos:', error);
